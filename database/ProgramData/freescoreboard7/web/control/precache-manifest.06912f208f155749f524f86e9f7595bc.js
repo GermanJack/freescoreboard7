e1ccbbf82661dffb9f709a44634779060bb3b82c
GermanJack <46387725+GermanJack@users.noreply.github.com>
2021-01-07 00:13:43 +0100

    initial upload

---
+++ database/ProgramData/freescoreboard7/web/control/precache-manifest.06912f208f155749f524f86e9f7595bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a4d9db73f7196f24146cc056a222ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "842dd75f0ff9fa825936",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cc39429272a965f5de9a",
+    "url": "./static/css/main.779d370b.chunk.css"
+  },
+  {
+    "revision": "842dd75f0ff9fa825936",
+    "url": "./static/js/2.3ab67459.chunk.js"
+  },
+  {
+    "revision": "342524d2763675921fb065e80074bdc5",
+    "url": "./static/js/2.3ab67459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc39429272a965f5de9a",
+    "url": "./static/js/main.7528be04.chunk.js"
+  },
+  {
+    "revision": "dd7fbfc338ef285790af",
+    "url": "./static/js/runtime-main.636f8da1.js"
+  }
+]);
---
+++ database/ProgramData/freescoreboard7/web/display/precache-manifest.1657119bf01e23adb24adbd5afc51490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc4b7a1bc287303a48cdd99b595103a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "14693f8464cf744e029a",
+    "url": "./static/css/main.30e45845.chunk.css"
+  },
+  {
+    "revision": "4cbaf0b86704d9cbb537",
+    "url": "./static/js/2.27f2f2b8.chunk.js"
+  },
+  {
+    "revision": "1f16231d73897228318688b0dff867ce",
+    "url": "./static/js/2.27f2f2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14693f8464cf744e029a",
+    "url": "./static/js/main.60a4fcd9.chunk.js"
+  },
+  {
+    "revision": "cf0066331a32b60ba793",
+    "url": "./static/js/runtime-main.a414466a.js"
+  }
+]);
